f1e9657c7a275f0e83ef0941f9e062b221fbacdb
Debora <debora.roldanlara@gmail.com>
2024-08-29 17:12:34 -0300

    Tipos de comentario

--- NomeFuncaoJava.java
+++ NomeFuncaoJava.java
@@ -1,6 +1,10 @@
+//classe - comentario de linha 
+/*
+Comentario de bloco
+*/
 public class NomeFuncaoJava{
 	//metodo
-	public static void main (){
+	public static void main(String[] args){
 	  System.out.println("Olá, Java!");
 	}
 }
