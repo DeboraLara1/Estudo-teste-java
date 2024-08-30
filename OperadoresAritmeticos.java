fdddcd98afc3b358306f800f9a360fb1c87f80e8
Debora <debora.roldanlara@gmail.com>
2024-08-30 15:06:39 -0300

    Operadores Aritméticos

---
+++ OperadoresAritmeticos.java
@@ -0,0 +1,24 @@
+//Operadores aritméticos
+public class OperadoresAritmeticos{
+	public static void main(String[] args){
+	  int minhaIdade = 40;
+	  int suaIdade = 25;
+
+	  //adição
+	  int totalIdades = minhaIdade + suaIdade;
+	  System.out.println("Soma das idades: " + totalIdades);
+
+	 //Subtração
+	  int diferencaIdades = minhaIdade - suaIdade;
+	  System.out.println("Diferença entre as idades: " + diferencaIdades);
+
+	  //Multiplicação
+	  int dobroDaIdade = 2 * suaIdade;
+	  System.out.println("Dobro da sua idade: " + dobroDaIdade);
+
+	  //Divisão
+	  int metadeSuaIdade = suaIdade / 2;
+  	  System.out.println("Metade da sua idade: " + metadeSuaIdade);
+
+	}
+}
