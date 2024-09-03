03426275be5c63814576a826a8cb6e947a0e1617
Debora <debora.roldanlara@gmail.com>
2024-09-03 15:39:54 -0300

    Conversão Double Para Float

---
+++ ConversaoDoubleParaFloat.java
@@ -0,0 +1,9 @@
+public class ConversaoDoubleParaFloat{
+	public static void main(String[] args){
+        double x = 10.6;
+        float y = (float) x;
+
+        System.out.println("x ---> " + x);
+        System.out.println("y ---> " + y);
+	}
+}
