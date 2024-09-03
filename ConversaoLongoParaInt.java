cd66bc65252f603b9b20f958fc13564ab36a2c7c
Debora <debora.roldanlara@gmail.com>
2024-09-03 15:33:51 -0300

    Conversão Long para Int

---
+++ ConversaoLongoParaInt.java
@@ -0,0 +1,9 @@
+public class ConversaoLongoParaInt{
+	public static void main(String[] args){
+        long x = 10;
+        int y = (int) x;
+
+        System.out.println("x ---> " + x);
+        System.out.println("y ---> " + y);
+	}
+}
