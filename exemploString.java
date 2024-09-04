08816b16d9ac9ed9ae4b5b1e13439d78a9d4ba2d
Debora <debora.roldanlara@gmail.com>
2024-09-04 12:54:15 -0300

    Exemplo de variavel string

---
+++ exemploString.java
@@ -0,0 +1,12 @@
+public class exemploString{
+	public static void main(String[] args){
+        int x = 10;
+        int y = 5;
+
+        System.out.println("Resultado: ---> " + (x + y));
+
+        String nome = "Bernardo";
+        int idade = 2;
+        System.out.println(nome + " tem " + idade + " anos.");
+	}
+}
