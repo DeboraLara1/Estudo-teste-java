55835903bf1dc4506ab7a67f274f0b27232c41ea
Debora <debora.roldanlara@gmail.com>
2024-09-17 15:54:02 -0300

    Estrutura condicionarl Switch apartir do java 17

---
+++ estruturaCondicionarlSwitch2.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class estruturaCondicionarlSwitch2 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite um dia da semana (Ex: seg, ter, qua, etc)");
+        String diaSemana = entrada.nextLine();
+        String horarioFuncionamento;
+//Forma mais agil de fazer um switch sem precisar do breack, apartir do java 17
+        switch (diaSemana) {
+            case "seg" -> horarioFuncionamento = "Fechado";
+            case "ter", "qua", "qui", "sex" -> horarioFuncionamento = "08:00 às 18:00";
+            case "dom" -> horarioFuncionamento = "08:00 às 12:0";
+            default -> horarioFuncionamento = "Dia inválido";
+        }
+
+        System.out.printf("Gorario de funcionamento: %s. ", horarioFuncionamento);
+
+    }
+}
