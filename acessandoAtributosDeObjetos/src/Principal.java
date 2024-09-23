1b7d41f261dab8ebc0efd75cc46ebb604b741de8
Debora <debora.roldanlara@gmail.com>
2024-09-23 13:42:03 -0300

    Desafio: instanciando objetos e acessando os atributos

--- acessandoAtributosDeObjetos/src/Principal.java
+++ acessandoAtributosDeObjetos/src/Principal.java
@@ -1,13 +1,20 @@
 public class Principal {
     public static void main(String[] args) {
-        Carro meuCarro = new Carro();
+//        Carro meuCarro = new Carro();
+//
+//        meuCarro.anoFabricacao = 2021;
+//        meuCarro.cor = "Preto";
+//        meuCarro.modelo = "HR-V";
+//        meuCarro.fabricante = "Honda";
+//
+//        System.out.printf("Modelo %s%n", meuCarro.modelo);
+//        System.out.printf("Ano Fabricação %s%n", meuCarro.anoFabricacao);
 
-        meuCarro.anoFabricacao = 2021;
-        meuCarro.cor = "Preto";
-        meuCarro.modelo = "HR-V";
-        meuCarro.fabricante = "Honda";
+        //Desafio: instanciando objetos e acessando os atributos
+        Cachorro meuCachorro = new Cachorro();
+        meuCachorro.raca = "Akita";
+        meuCachorro.nome = "Linda";
+        System.out.printf("o cachorro %S é da raça %S", meuCachorro.nome, meuCachorro.raca);
 
-        System.out.printf("Modelo %s%n", meuCarro.modelo);
-        System.out.printf("Ano Fabricação %s%n", meuCarro.anoFabricacao);
     }
 }
