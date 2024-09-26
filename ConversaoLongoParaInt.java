fb7c31d44c9edcc2c6d26f5121b75bc7e8c1d894
Debora <debora.roldanlara@gmail.com>
2024-09-26 16:42:33 -0300

    ConversaoLongoParaInt

--- ConversaoLongoParaInt.java
+++
@@ -1,9 +0,0 @@
-public class ConversaoLongoParaInt{
-	public static void main(String[] args){
-        long x = 10;
-        int y = (int) x;
-
-        System.out.println("x ---> " + x);
-        System.out.println("y ---> " + y);
-	}
-}
